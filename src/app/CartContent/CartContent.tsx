da007be3d5b6ab98b1c2987676b92a49620c78ed
Aditya kumar singh <kumaraditya30102003@gmail.com>
2024-10-04 10:45:12 +0530

    fixing deployment

--- src/app/CartContent/CartContent.tsx
+++ src/app/CartContent/CartContent.tsx
@@ -1,7 +1,6 @@
 
 'use client';
 
-import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../Redux/Store';
 import Link from 'next/link';
